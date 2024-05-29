1c6a48f5492615e23aa8de15f4293e581a528c8e
bibyan06 <vivianprimavera.vivo@bicol-u.edu.ph>
2024-05-30 02:15:32 +0800

    CsharpApplicationFiles

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace CsharpApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.accounts = new System.Windows.Forms.DataGridView();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTotalSalary = new System.Windows.Forms.TextBox();
+            this.departmentsComboBox = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPost = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.accounts)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.accounts);
+            this.groupBox1.Controls.Add(this.btnGet);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(537, 245);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // accounts
+            // 
+            this.accounts.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.accounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.accounts.Location = new System.Drawing.Point(26, 16);
+            this.accounts.Name = "accounts";
+            this.accounts.RowHeadersWidth = 51;
+            this.accounts.RowTemplate.Height = 24;
+            this.accounts.Size = new System.Drawing.Size(484, 150);
+            this.accounts.TabIndex = 5;
+            // 
+            // btnGet
+            // 
+            this.btnGet.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnGet.FlatAppearance.BorderColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnGet.FlatAppearance.BorderSize = 0;
+            this.btnGet.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGet.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGet.Location = new System.Drawing.Point(26, 172);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(484, 44);
+            this.btnGet.TabIndex = 3;
+            this.btnGet.Text = "GET";
+            this.btnGet.UseVisualStyleBackColor = false;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtTotalSalary);
+            this.groupBox2.Controls.Add(this.departmentsComboBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.btnPost);
+            this.groupBox2.Controls.Add(this.txtEmail);
+            this.groupBox2.Controls.Add(this.txtPassword);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtUsername);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 263);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(536, 274);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtTotalSalary
+            // 
+            this.txtTotalSalary.Location = new System.Drawing.Point(155, 170);
+            this.txtTotalSalary.Name = "txtTotalSalary";
+            this.txtTotalSalary.Size = new System.Drawing.Size(354, 22);
+            this.txtTotalSalary.TabIndex = 10;
+            // 
+            // departmentsComboBox
+            // 
+            this.departmentsComboBox.FormattingEnabled = true;
+            this.departmentsComboBox.Location = new System.Drawing.Point(155, 136);
+            this.departmentsComboBox.Name = "departmentsComboBox";
+            this.departmentsComboBox.Size = new System.Drawing.Size(184, 24);
+            this.departmentsComboBox.TabIndex = 9;
+            this.departmentsComboBox.SelectedIndexChanged += new System.EventHandler(this.DepartmentsComboBox_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Total Salary:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Department Name:";
+            // 
+            // btnPost
+            // 
+            this.btnPost.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnPost.FlatAppearance.BorderColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnPost.FlatAppearance.BorderSize = 0;
+            this.btnPost.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPost.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPost.Location = new System.Drawing.Point(25, 210);
+            this.btnPost.Name = "btnPost";
+            this.btnPost.Size = new System.Drawing.Size(484, 49);
+            this.btnPost.TabIndex = 6;
+            this.btnPost.Text = "POST";
+            this.btnPost.UseVisualStyleBackColor = false;
+            this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(110, 101);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(399, 22);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(110, 66);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(399, 22);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(110, 29);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(399, 22);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 549);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Activity 8 - API Endpoint";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.accounts)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPost;
+        private System.Windows.Forms.TextBox txtTotalSalary;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox departmentsComboBox;
+        private System.Windows.Forms.DataGridView accounts;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace CsharpApi
+{
+    public partial class Form1 : Form
+    {
+        private List<Department> departments;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDepartments();
+            departmentsComboBox.SelectedIndexChanged += DepartmentsComboBox_SelectedIndexChanged;
+        }
+
+        // POST Method Process
+        private async void btnPost_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string email = txtEmail.Text;
+            int dname = departments[departmentsComboBox.SelectedIndex].dnumber;
+            int totalsalary = dname;
+
+            var userData = new
+            {
+                username = username,
+                pass = password,
+                email = email,
+                dname = dname,
+                totalsalary = totalsalary
+            };
+
+            var jsonData = JsonConvert.SerializeObject(userData);
+
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("http://localhost/Act8_API/api.php", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("User added successfully");
+                    ClearInputFields();
+                }
+                else
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show($"Failed to add user: {responseContent}");
+                }
+            }
+        }
+
+        // GET Method Process
+        private async void btnGet_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://localhost/Act8_API/api.php?action=getUsers");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    try
+                    {
+                        var users = JsonConvert.DeserializeObject<List<User>>(jsonData);
+
+                        DataTable dataTable = new DataTable();
+                        dataTable.Columns.Add("Username");
+                        dataTable.Columns.Add("Password");
+                        dataTable.Columns.Add("Email");
+                        dataTable.Columns.Add("Department Name");
+                        dataTable.Columns.Add("Total Salary");
+
+                        foreach (var user in users)
+                        {
+                            dataTable.Rows.Add(user.username, user.pass, user.email, user.dname, user.totalsalary);
+                        }
+
+                        accounts.DataSource = dataTable;
+                    }
+                    catch (JsonException ex)
+                    {
+                        MessageBox.Show($"Error parsing user data: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show($"Failed to fetch data from the server: {responseContent}");
+                }
+            }
+        }
+
+        // Load Departments
+        private async void LoadDepartments()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://localhost/Act8_API/api.php?action=getDepartments");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    try
+                    {
+                        departments = JsonConvert.DeserializeObject<List<Department>>(jsonData);
+
+                        departmentsComboBox.Items.Clear();
+
+                        foreach (var department in departments)
+                        {
+                            departmentsComboBox.Items.Add(department.dname);
+                        }
+                    }
+                    catch (JsonException ex)
+                    {
+                        MessageBox.Show($"Error parsing department data: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show($"Failed to fetch departments from the server: {responseContent}");
+                }
+            }
+        }
+
+        // Department Selection Changed
+        private async void DepartmentsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (departmentsComboBox.SelectedIndex == -1)
+                return;
+
+            var selectedDepartment = departments[departmentsComboBox.SelectedIndex];
+            int dnumber = selectedDepartment.dnumber;
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://localhost/Act8_API/api.php?action=getTotalSalary&dnumber={dnumber}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    try
+                    {
+                        var totalsalary = JsonConvert.DeserializeObject<TotalSalary>(jsonData);
+                        txtTotalSalary.Text = totalsalary.totalsalary.ToString();
+                    }
+                    catch (JsonException ex)
+                    {
+                        MessageBox.Show($"Error parsing total salary data: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show($"Failed to fetch total salary from the server: {responseContent}");
+                }
+            }
+        }
+
+        private void ClearInputFields()
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtPassword.Text = "";
+            txtEmail.Text = "";
+            txtTotalSalary.Text = "";
+        }
+
+        class TotalSalary
+        {
+            public int totalsalary { get; set; }
+        }
+
+        public class User
+        {
+            public string username { get; set; }
+            public string pass { get; set; }
+            public string email { get; set; }
+            public string dname { get; set; }
+            public int totalsalary { get; set; }
+        }
+
+        public class Department
+        {
+            public string dname { get; set; }
+            public int dnumber { get; set; }
+        }
+    }
+}
